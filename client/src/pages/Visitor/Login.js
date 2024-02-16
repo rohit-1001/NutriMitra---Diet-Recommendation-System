ff440198e57004fca393454f0a52b937b4b916c8
rohit <rohitphalke2003@gmail.com>
2024-02-16 20:32:52 +0530

    Added user model

--- client/src/pages/Visitor/Login.js
+++ client/src/pages/Visitor/Login.js
@@ -1,35 +1,94 @@
-import React from 'react'
-import '../../css_files/login.css'
+import React, {useState} from "react";
+import "../../css_files/login.css";
+const axios = require("axios");
 
 const Login = () => {
+  const [login, setLogin] = useState({
+    email: "",
+    password: ""
+  });
+
+  const [register, setRegister] = useState({
+    name: "",
+    email: "",
+    password: ""
+  });
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(login);
+  };
+
   return (
     <>
-        <div className="login_container">
-        <div className="login_main">  	
-            <input type="checkbox" id="login_chk" aria-hidden="true"/>
-    
-            <div className="login_login">
-                <form className="login_form">
-                    <label className='login_label' htmlFor="login_chk" aria-hidden="true">Log in</label>
-                    <input className="login_input" type="email" name="email" placeholder="Email" required />
-                    <input className="login_input" type="password" name="pswd" placeholder="Password" required />
-                    <button>Log in</button>
-                </form>
-            </div>
-    
-            <div className="login_register">
-                <form className="login_form">
-                    <label className='login_label' htmlFor="login_chk" aria-hidden="true">Register</label>
-                    <input className="login_input" type="text" name="txt" placeholder="Username" required />
-                    <input className="login_input" type="email" name="email" placeholder="Email" required />
-                    <input className="login_input" type="password" name="pswd" placeholder="Password" required />
-                    <button>Register</button>
-                </form>
-            </div>
-        </div>
-    </div>
-    </>
-  )
-}
+      <div className="login_container">
+        <div className="login_main">
+          <input type="checkbox" id="login_chk" aria-hidden="true" />
 
-export default Login
+          <div className="login_login">
+            <form className="login_form" onSubmit={handleLogin}>
+              <label
+                className="login_label"
+                htmlFor="login_chk"
+                aria-hidden="true"
+              >
+                Log in
+              </label>
+              <input
+                className="login_input"
+                type="email"
+                name="email"
+                placeholder="Email"
+                required
+              />
+              <input
+                className="login_input"
+                type="password"
+                name="pswd"
+                placeholder="Password"
+                required
+              />
+              <button>Log in</button>
+            </form>
+          </div>
+
+          <div className="login_register">
+            <form className="login_form">
+              <label
+                className="login_label"
+                htmlFor="login_chk"
+                aria-hidden="true"
+              >
+                Register
+              </label>
+              <input
+                className="login_input"
+                type="text"
+                name="txt"
+                placeholder="Name"
+                required
+              />
+              <input
+                className="login_input"
+                type="email"
+                name="email"
+                placeholder="Email"
+                required
+              />
+              <input
+                className="login_input"
+                type="password"
+                name="pswd"
+                placeholder="Password"
+                required
+              />
+              <button>Register</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,48 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role:{
+        type: String,
+        default: null,
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true,
+        }
+    }]
+});
+
+UserSchema.pre('save', async function (next) {
+    const user = this;
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+    next();
+}
+);
+
+UserSchema.methods.generateAuthToken = async function () {
+    const user = this;
+    const token = jwt.sign({ _id: user._id, email: user.email}, process.env.SECRET_KEY);
+    user.tokens = user.tokens.concat({ token });
+    await user.save();
+    return token;
+}
+
+module.exports = mongoose.model('User', UserSchema);
