e1b0f6ac00f675eeae881c9a2cbc6b41c855daf3
rohit <rohitphalke2003@gmail.com>
2024-02-18 17:09:08 +0530

    Removed print statements

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -10,13 +10,9 @@ const APP_KEY = process.env.RECIPE_APP_KEY;
 
 
 const getRecipe = async (req, res) => {
-    // console.log("Hello");
     const { query } = req.body;
-    // console.log(query);
     try { 
-        // console.log("Hello")
         const response = await axios.get(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
-        // console.log(response.data.hits);
         return res.json(response.data.hits);
     } catch (error) {
         console.error('Error fetching recipes:', error);
